0a214fbe8ff82e7bedbb6ef7eeabc3202de074bb
JonathanProf <62577666+JonathanProf@users.noreply.github.com>
2024-01-07 17:31:40 +0000

    Se realiza la extracción de cuales dispositivos son inoperables por misión

--- sistema_monitoreo_Apollo_11/sistema_monitoreo_Apollo_11/utilities/ReportGeneration.py
+++ sistema_monitoreo_Apollo_11/sistema_monitoreo_Apollo_11/utilities/ReportGeneration.py
@@ -56,9 +56,9 @@ class ReportGeneration:
         
         dict_report = self.dataframe.groupby(by=['mission', 'device_type', 'device_status'])['device_status'].count().to_dict()
 
-        msg = f'{"="*50}\n'
-        msg += "Analisis de eventos por estado para cada misión y dispositivo\n"
-        
+        msg = f'\n{"="*100}\n'
+        msg += "Analisis de eventos por estado para cada misión y dispositivo".center(100)
+        msg += f'\n{"="*100}\n\n'
         
         table = []
         for k, event_occurrence_number in dict_report.items():
@@ -66,8 +66,7 @@ class ReportGeneration:
             row.append(event_occurrence_number)
             table.append(row)
 
-        msg += tabulate(table, headers=['Mission', 'Device Type', 'Device Status', 'Event Occurrence Number'], tablefmt="grid")
-        
+        msg += tabulate(table, headers=['Mission', 'Device Type', 'Device Status', 'Number Unknown Devices'], tablefmt="grid")
         
         logging.info(msg)
         return msg
@@ -83,15 +82,40 @@ class ReportGeneration:
 
         df2 = self.dataframe[self.dataframe["device_status"] == 'unknown']
 
-        msg = f'Cantidad de desconexiones encontradas {len(df2)}\n'
-        msg += f'{"="*50}\n'
-        msg += "Desconexiones reportadas por misión\n"
-        msg += f'{"="*50}\n'
+        msg = f'\n{"="*100}\n'
+        msg = f'Cantidad de desconexiones encontradas {len(df2)}'.center(100)
+        msg += f'\n{"="*100}\n'
+        msg += "Desconexiones reportadas por misión".center(100)
+        msg += f'\n{"="*100}\n'
 
         for device, value in df2.groupby('device_type')['device_status'].count().to_dict().items():
             msg += f'Hay {value} desconexiones para el dispositivo {device}\n'
         
-        msg += f'{"="*50}\n'
+        msg += f'\n{"="*100}\n'
         
         logging.info(msg)
         return msg       
+
+    def mission_consolidation(self) -> str:
+        """
+        d) Consolidación de misiones
+        Debe realizarse la consolidación de todas las misiones para determinar
+        cuántos dispositivos son inoperables.
+        """
+        df_consolidation = self.dataframe[self.dataframe["device_status"] == 'killed']
+        dict_report = df_consolidation.groupby(['mission', 'device_type'])['device_status'].count().to_dict()
+
+        table = []
+        msg = f'\n\n{"="*100}\n'
+        msg += "Consolidación de misiones para dispositivos inoperables".center(100)
+        msg += f'\n{"="*100}\n'
+        for k, event_occurrence_number in dict_report.items():
+            row = list(k)
+            row.append(event_occurrence_number)
+            table.append(row)
+
+        msg += tabulate(table, headers=['Mission', 'Device Type', 'Number Killed Devices'], tablefmt="grid")
+        msg += f'\n{"="*100}\n'
+
+        logging.info(msg)
+        return msg
